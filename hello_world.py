dd0aa4e7e5ae796b24fb5f0793abdbdd22dfc2e5
yordan <yordan@infinitelambda.com>
2022-03-24 17:06:57 +0200

    "Adding a line

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,7 @@
 print("Hello World from the python file")
 print("This line was adde as editing from remote repo")
+<<<<<<< HEAD
 # This line is created just to make conflict.
+=======
+# This is just a non sence line for example.xy
+>>>>>>> 245e2c2 (Adding a line)
